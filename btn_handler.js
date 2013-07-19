352e651353ebda079bd325f068eb445e973ebddc
Emile Esterhuizen <emile.app@gmail.com>
2013-07-19 11:47:52 +0200

    font and style changes

--- btn_handler.js
+++ btn_handler.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
-    var initial_btn_color = '#FFFF40';
-    var touched_btn_color = '#A6A600';
+    var initial_btn_color = '#2E2E26';
+    var touched_btn_color = '#6B6B55';
     
 
     $('body').on('tap click', '#toc_icon_show', function (){
@@ -9,19 +9,19 @@ $(document).ready(function(){
     });
     
     $('body').on('touchstart mousedown', '.btn', function(){
-        $(this).css('background-color', touched_btn_color);
+        $(this).css('color', touched_btn_color);
     });
     
     $('body').on('touchend mouseup', '.btn', function(){
-        $(this).css('background-color', initial_btn_color);
+        $(this).css('color', initial_btn_color);
     });
     
     $('body').on('touchstart mousedown', '.article', function(){
-        $(this).css('background-color', touched_btn_color);
+        $(this).css('color', touched_btn_color);
     });
     
     $('body').on('touchend mouseup', '.article', function(){
-        $(this).css('background-color', initial_btn_color);
+        $(this).css('color', initial_btn_color);
     });
     
 });
