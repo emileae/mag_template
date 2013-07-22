969c10064634fd2aa565defdb9f6b67bc2717046
Emile Esterhuizen <emile.app@gmail.com>
2013-07-22 16:41:58 +0200

    debug

--- no_menu_handler.js
+++ no_menu_handler.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(document).on('touchstart', '.no_menu', function(){
         menu_active = false;
+        alert(menu_active);
     });
     
     $(document).on('touchend', '.no_menu', function(){
